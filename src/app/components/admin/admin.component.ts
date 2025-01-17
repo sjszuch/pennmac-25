8a2c5f4c7d64d10805d74687e523037a244f8cf8
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2025-01-16 23:31:28 -0500

    Added Firestore read

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, Inject, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
-import { Firestore } from '@angular/fire/firestore';
-import {addDoc, collection} from 'firebase/firestore';
+import { Firestore, collection, addDoc, getDocs } from '@angular/fire/firestore';
+import { db } from '../../../environments/environment';
 
 @Component({
   selector: 'app-admin',
@@ -15,11 +15,10 @@ export class AdminComponent {
 
   @ViewChild('productForm') productForm: any;
   firestore: Firestore = inject(Firestore);
-  
 
   async saveData() {
     const acollection = collection(this.firestore, '/products');
-    addDoc(acollection, {
+    await addDoc(acollection, {
       'name': this.productForm.value.name,
       'price': this.productForm.value.price,
       'description': this.productForm.value.description,
@@ -37,13 +36,22 @@ export class AdminComponent {
     });
   }
 
-  // This function submits the user's form and resets it
-  submitProduct() {
-    alert('Product submitted');
-    alert(this.productForm.value.name + ' ' + this.productForm.value.price + ' ' + this.productForm.value.description); 
-    this.saveData();
-    this.resetForm();
+  // This function logs all products in the Firestore database to the console
+  async logAllProducts() {
+    const acollection = collection(this.firestore, '/products');
+    const snapshot = await getDocs(acollection);
+    const products = snapshot.docs.map(doc => doc.data());
+    console.log(products);
   }
 
+  // This function submits the user's form and resets it
+  async submitProduct() {
+    alert('Product submitted');
+    alert(this.productForm.value.name + ' ' + this.productForm.value.price + ' ' + this.productForm.value.description); 
+    await this.saveData();
+    this.resetForm();
+    this.logAllProducts();
+  }
 
+  
 }
--- src/app/components/shop-page/shop-page.component.ts
+++ src/app/components/shop-page/shop-page.component.ts
@@ -1,21 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewChild, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Firestore, collection, addDoc, getDocs } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-shop-page',
   standalone: true,
   imports: [ProductCardComponent, CommonModule],
   templateUrl: './shop-page.component.html',
-  styleUrl: './shop-page.component.scss'
+  styleUrls: ['./shop-page.component.scss']
 })
-export class ShopPageComponent {
+export class ShopPageComponent implements OnInit {
 
-  products!: any[];
+  products: any[] = [];
 
-  constructor() {
-    fetch('assets/dummy.json')
-      .then(res => res.json())
-      .then(data => this.products = data.products);
+  @ViewChild('productForm') productForm: any;
+  firestore: Firestore = inject(Firestore);
+
+  async ngOnInit() {
+    await this.loadAllProducts();
+  }
+
+  async saveData() {
+    const acollection = collection(this.firestore, '/products');
+    await addDoc(acollection, {
+      'name': this.productForm.value.name,
+      'price': this.productForm.value.price,
+      'description': this.productForm.value.description,
+      'image': this.productForm.value.image
+    });
+  }
+
+  resetForm() {
+    this.productForm.reset({
+      name: '',
+      price: '',
+      description: '',
+      image: ''
+    });
+  }
+
+  async loadAllProducts() {
+    const acollection = collection(this.firestore, '/products');
+    const snapshot = await getDocs(acollection);
+    this.products = snapshot.docs.map(doc => doc.data());
+    console.log(this.products);
+  }
+
+  async submitProduct() {
+    alert('Product submitted');
+    alert(this.productForm.value.name + ' ' + this.productForm.value.price + ' ' + this.productForm.value.description); 
+    await this.saveData();
+    this.resetForm();
+    await this.loadAllProducts();
   }
 }
